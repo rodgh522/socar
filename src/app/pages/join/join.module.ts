ca90d256a76364e7576569aa16b1425eca7430ce
gyeongho <rodgh522@gmail.com>
2022-07-29 07:00:20 +0900

    distinguish join process 	modified:   src/app/pages/join/join.component.html 	modified:   src/app/pages/join/join.component.scss 	modified:   src/app/pages/join/join.module.ts 	new file:   src/app/pages/join/step1/step1.component.html 	new file:   src/app/pages/join/step1/step1.component.scss 	new file:   src/app/pages/join/step1/step1.component.ts 	new file:   src/assets/images/icons/left-arrow.png 	new file:   src/assets/images/join_triangle.png

--- src/app/pages/join/join.module.ts
+++ src/app/pages/join/join.module.ts
@@ -1,22 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { JoinComponent } from './join.component';
 import { JoinService } from './join.service';
 import { NativeScriptRouterModule, NativeScriptCommonModule } from '@nativescript/angular';
 import { Routes } from '@angular/router';
+import { SharedModule } from '~/app/@core/shared.module';
+import { Step1Component } from './step1/step1.component';
+import { AgreementComponent } from './agreement/agreement.component';
 
 const routes: Routes = [
-  { path: '', component: JoinComponent }
+  { path: '', component: Step1Component },
+  { path: 'agreement', component: AgreementComponent },
 ];
 
 @NgModule({
   declarations: [
-    JoinComponent
+    JoinComponent,
+    Step1Component,
+    AgreementComponent
   ],
   imports: [
     CommonModule, NativeScriptCommonModule, 
-    NativeScriptRouterModule.forChild(routes)
+    NativeScriptRouterModule.forChild(routes),
+    SharedModule
   ],
+  schemas: [NO_ERRORS_SCHEMA],
   providers: [ JoinService ]
 })
 export class JoinModule { }
